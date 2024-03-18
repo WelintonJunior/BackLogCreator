cd1014fed1a1f2da9b2d6b6f62069fbd4b18a34e
WelintonJunior <welintonjr011@gmail.com>
2024-03-18 09:23:37 -0300

    FrontEnd main ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form>
+          <div className="row">
+            <div className="col-sm-12 col-md-4 d-flex align-items-center p-0">
+              <label htmlFor="perfil" className="mx-2">
+                Como
+              </label>
+              <select
+                name="perfil"
+                defaultValue={"Cliente"}
+                className="form-select"
+              >
+                <option value="Cliente">Cliente</option>
+                <option value="Administrador">Administrador</option>{" "}
+              </select>
+            </div>
+            <span className="col-sm-12 col-md-4 d-flex align-items-center p-0">
+              <label htmlFor="quero" className="mx-2">
+                quero
+              </label>
+              <input name="quero" type="text" className="form-control" />
+            </span>
+            <span className="col-sm-12 col-md-4 d-flex align-items-center p-0">
+              <label htmlFor="para" className="mx-2">
+                para
+              </label>
+              <input type="text" name="para" className="form-control" />
+            </span>
+          </div>
+          <br />
+          <div className="text-center">
+            <button type="submit">Enviar</button>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
